2721981490e611df13b9a9e18d83eb5c86820ce6
lareina-Ipse <lareina.ipse@gmail.com>
2019-12-05 09:36:58 +0900

    카이언스 Beacon 스캔 설정 & 퍼미션 (사전작업) 블루투스 설정 On / Off : BluetoothAdapter -> isEnabled() 위치정보 설정 On / Off (OS Nougot 이상일 경우에만) LocationManager -> isProviderEnabled(...) 블루투스 스캐닝 위한 퍼미션 : Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
    beacon 스캔 시작
    롤리팝 이상 : BluetoothLeScanner -> startScan(...)
    롤리팝 미만 : BluetoothAdapter -> startLeScan(...)
    
    beacon 스캔 중지
    롤리팝 이상 : BluetoothLeScanner -> stopScan(...)
    롤리팝 미만 : BluetoothAdapter -> stopLeScan(...)
    
    미션???
    1) 액티비티에 버튼 2개 (스캔 시작, 스캔 중지) / 리스트뷰 or 리사이클러뷰
    ex)
    [ 스캔시작 버튼]
    [ 스캔중비 버튼]
    [                  ]
    [   리스트뷰    ]
    [                  ]
    [                  ]
    
    2) 스캔시작 버튼 누르면 스캔된 Beacon 정보 리스트뷰 or 리사이클러뷰에 표시
    3) 리스트뷰 or 리사이클러뷰 아이템에는 Beacon 이름, 맥어드레스, rssi, uuid 표시
    3) 스캔중비 버튼 누르면 스캔중지

---
+++ app/src/main/java/com/example/bluetoothex/BaseInterface.java
@@ -0,0 +1,11 @@
+package com.example.bluetoothex;
+
+public interface BaseInterface {
+
+    public void initViews();
+    public void initListener();
+    public void initItems();
+    public void initProcess();
+
+
+}
---
+++ app/src/main/java/com/example/bluetoothex/ScanAdapter.java
@@ -0,0 +1,73 @@
+package com.example.bluetoothex;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ScanAdapter extends RecyclerView.Adapter<ScanAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<Scan> mList;
+    private LayoutInflater mInflater;
+
+    public ScanAdapter(ArrayList<Scan> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+        this.mInflater = mInflater;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_scan, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.textview_name.setText(mList.get(position).getName());
+        holder.textview_address.setText(mList.get(position).getAddress());
+        holder.textView_UUID.setText(mList.get(position).getUuid());
+        holder.textView_rssi.setText(mList.get(position).getRssi());
+
+    }
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textview_name;
+        TextView textview_address;
+        TextView textView_rssi;
+        TextView textView_UUID;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.textview_name = itemView.findViewById(R.id.textview_name);
+            this.textview_address = itemView.findViewById(R.id.textview_address);
+            this.textView_rssi = itemView.findViewById(R.id.textview_rssi);
+            this.textView_UUID = itemView.findViewById(R.id.textview_uuid);
+
+        }
+    }
+
+}
